423f7a1b1b8d9df22897b2f07bcd338deb01b9af
Hyan Batista <hyanbatista42@gmail.com>
2024-02-26 12:27:37 +0100

    feat: optimize RF

--- hyperparamenter-optimization.py
+++ hyperparamenter-optimization.py
@@ -1,13 +1,13 @@
+import dataclasses as dc
 import random
+import warnings
 from functools import cached_property, partial
 from typing import Literal, Self, Sequence
-from deap import base, algorithms, creator, tools
+
+import numpy as np
+from deap import algorithms, base, creator, tools
 from sklearn import datasets, metrics, model_selection, neural_network
 from tqdm.contrib.concurrent import process_map
-import numpy as np
-import dataclasses as dc
-
-import warnings
 
 MIN_LENGTH = 0
 MAX_LENGTH = 8
@@ -15,29 +15,39 @@ MIN_NEURONS = 2
 MAX_NEURONS = 8
 RANDOM_STATE = 42
 
+
 def float_field(start: float, end: float):
     def factory():
-        return start + random.random()*(end-start)
+        return start + random.random() * (end - start)
+
     return dc.field(default_factory=factory)
+
 
 def list_field() -> list[int]:
     def factory() -> list[int]:
-        return [random.randint(MIN_NEURONS, MAX_NEURONS) for _ in range(random.randint(MIN_LENGTH, MAX_LENGTH))]
+        return [
+            random.randint(MIN_NEURONS, MAX_NEURONS)
+            for _ in range(random.randint(MIN_LENGTH, MAX_LENGTH))
+        ]
+
     return dc.field(default_factory=factory)
+
 
 def int_field(start: float, end: float):
     return dc.field(default_factory=partial(random.randint, start, end))
 
+
 def str_field(*options: str) -> str:
     return dc.field(default_factory=partial(random.choice, options))
+
 
 @dc.dataclass(slots=True)
 class Individual:
     hidden_layer_sizes: list = list_field()
-    activation: str = str_field('relu', 'identity', 'logistic', 'tanh')
-    solver: str  = str_field('lbfgs', 'sgd', 'adam')
+    activation: str = str_field("relu", "identity", "logistic", "tanh")
+    solver: str = str_field("lbfgs", "sgd", "adam")
     alpha: float = float_field(0.0001, 0.1)
-    learning_rate: str = str_field('constant', 'invscaling', 'adaptive')
+    learning_rate: str = str_field("constant", "invscaling", "adaptive")
     learning_rate_init: float = float_field(0.001, 0.1)
     power_t: float = float_field(0.2, 0.8)
     momentum: float = float_field(0.2, 0.9)
@@ -50,8 +60,8 @@ class Individual:
         return neural_network.MLPClassifier(
             random_state=RANDOM_STATE,
             max_iter=300,
-            hidden_layer_sizes=self.hidden_layer_sizes, 
-            activation=self.activation, 
+            hidden_layer_sizes=self.hidden_layer_sizes,
+            activation=self.activation,
             solver=self.solver,
             alpha=self.alpha,
             learning_rate=self.learning_rate,
@@ -60,7 +70,7 @@ class Individual:
             momentum=self.momentum,
             beta_1=self.beta_1,
             beta_2=self.beta_2,
-            epsilon=self.epsilon
+            epsilon=self.epsilon,
         )
 
     def mutate(self, indpb: float = 0.2):
@@ -74,15 +84,35 @@ class Individual:
     def mate_onepoint(self, other):
         field_names = [field.name for field in dc.fields(self)]
         point = random.randint(0, len(field_names))
-        child1 = type(self)(**{attr: getattr(self if i < point else other, attr) for i, attr in enumerate(field_names)})
-        child2 = type(self)(**{attr: getattr(other if i < point else self, attr) for i, attr in enumerate(field_names)})
+        child1 = type(self)(
+            **{
+                attr: getattr(self if i < point else other, attr)
+                for i, attr in enumerate(field_names)
+            }
+        )
+        child2 = type(self)(
+            **{
+                attr: getattr(other if i < point else self, attr)
+                for i, attr in enumerate(field_names)
+            }
+        )
         return child1, child2
 
     def mate_oneattr(self, other):
         field_names = [field.name for field in dc.fields(self)]
         point = random.randint(0, len(field_names))
-        child1 = type(self)(**{attr: getattr(self if i == point else other, attr) for i, attr in enumerate(field_names)})
-        child2 = type(self)(**{attr: getattr(other if i == point else self, attr) for i, attr in enumerate(field_names)})
+        child1 = type(self)(
+            **{
+                attr: getattr(self if i == point else other, attr)
+                for i, attr in enumerate(field_names)
+            }
+        )
+        child2 = type(self)(
+            **{
+                attr: getattr(other if i == point else self, attr)
+                for i, attr in enumerate(field_names)
+            }
+        )
         return child1, child2
 
     def evaluate(self, x_train, x_test, y_train, y_test):
@@ -92,23 +122,35 @@ class Individual:
         size_error = sum(self)
         return error, size_error
 
+
 creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0))
 creator.create("Individual", Individual, fitness=creator.FitnessMin)
 
+
 def main(ngen: int, population: int, halloffame: int = 5):
-    print('Carregando Dataset')
+    print("Carregando Dataset")
     # X, y = datasets.make_classification(5000)
     X, y = datasets.load_digits(return_X_y=True)
-    x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(
+        X, y, test_size=0.2
+    )
 
-    evaluate = partial(Individual.evaluate, x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
+    evaluate = partial(
+        Individual.evaluate,
+        x_train=x_train,
+        x_test=x_test,
+        y_train=y_train,
+        y_test=y_test,
+    )
 
-    print('Construindo Toolbox')
+    print("Construindo Toolbox")
     toolbox = base.Toolbox()
     toolbox.register("individual", creator.Individual)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("mate", tools.cxMessyOnePoint)
-    toolbox.register("mutate", tools.mutUniformInt, low=MIN_NEURONS, up=MAX_NEURONS, indpb=0.2)
+    toolbox.register(
+        "mutate", tools.mutUniformInt, low=MIN_NEURONS, up=MAX_NEURONS, indpb=0.2
+    )
     toolbox.register("select", tools.selTournament, tournsize=5)
     toolbox.register("evaluate", evaluate)
     toolbox.register("map", process_map, max_workers=6, chunksize=8, ncols=80)
@@ -116,15 +158,20 @@ def main(ngen: int, population: int, halloffame: int = 5):
     pop = toolbox.population(n=population)
     hof = tools.HallOfFame(halloffame)
     stats = tools.Statistics()
-    stats.register('min', lambda pop: min(ind.fitness.values for ind in pop))
+    stats.register("min", lambda pop: min(ind.fitness.values for ind in pop))
 
-    print('Iniciando Algoritmo')
-    pop, log = algorithms.eaSimple(pop, toolbox, 0.2, 0.2, ngen=ngen, halloffame=hof, stats=stats)
+    print("Iniciando Algoritmo")
+    pop, log = algorithms.eaSimple(
+        pop, toolbox, 0.2, 0.2, ngen=ngen, halloffame=hof, stats=stats
+    )
     print(log)
     for ind in hof:
         error, size = ind.fitness.values
-        print('Error: %s, Size: %s' % (error, size))
-    print('Best model hidden layers: %s - error: %d' % (hof[0], hof[0].fitness.values[0]))
+        print("Error: %s, Size: %s" % (error, size))
+    print(
+        "Best model hidden layers: %s - error: %d" % (hof[0], hof[0].fitness.values[0])
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(ngen=48, population=24)
